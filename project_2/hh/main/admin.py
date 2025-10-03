f900eb67633599159563f0ba5ff2e30efadba949
Mikael Hug <mikael.hug@helicon.ai>
2025-10-03 15:11:41 +0200

    list and filter

--- project_2/hh/main/admin.py
+++ project_2/hh/main/admin.py
@@ -12,10 +12,11 @@ from .models import Location, Sensor
 # decorator shorthand for registering a model together with a custom ModelAdmin class
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug", "id")
+    list_display = ("name", "slug")
     prepopulated_fields: ClassVar[dict[str, tuple[str, ...]]] = {"slug": ("name",)}  # type: ignore
 
 
 @admin.register(Sensor)
 class SensorAdmin(admin.ModelAdmin):
     list_display = ("name", "location", "id")
+    list_filter = ("location",)
