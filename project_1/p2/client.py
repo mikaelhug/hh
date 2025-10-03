74bbf3c0d8bb5a611231fbf096dbc25a68bd8fd8
Mikael Hug <mikael.hug@helicon.ai>
2025-10-03 09:53:03 +0200

    project 1 finished

--- project_1/p2/client.py
+++ project_1/p2/client.py
@@ -1,10 +1,26 @@
+import base64
+import random
 import socket
+import struct
+import time
 
-HOST = "127.0.0.1"    # The remote host
-PORT = 50007              # The same port as used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"NAkAALoLAAA=")
-    data = s.recv(1024)
+BASE64_SIZE = 12
 
-print('Received', repr(data))
+while True:
+    # generate random temperature and humidity
+    rand_tem = random.randint(-5000, 12000)  # noqa: S311
+    rand_hum = random.randint(0, 10000)  # noqa: S311
+    print(f"\n** Sending **\nTemperature: {rand_tem / 100} C\nHumidity: {rand_hum / 100} %")
+    packed = struct.pack("<ii", rand_tem, rand_hum)
+    encoded = base64.b64encode(packed)
+
+    HOST = "127.0.0.1"  # The remote host
+    PORT = 50007  # The same port as used by the server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(encoded)
+        data = s.recv(BASE64_SIZE)
+
+    if repr(data) != "b'Ok'":
+        print("Received", repr(data))
+    time.sleep(10)
--- project_1/p2/server.py
+++ project_1/p2/server.py
@@ -6,13 +6,41 @@ import threading
 HOST = "127.0.0.1"  # Symbolic name meaning all available interfaces
 PORT = 50007  # Arbitrary non-privileged port
 PACKET_SIZE = 8
+BASE64_SIZE = 12
 BASE = 10
 EXPONENT = -2
+
+# Endianness
+# Different architectures interpret data differently
+#   < - little endian: lowest order byte first (x86, x86_64, Arm)
+#   > - big endian: highest order byte comes first (PowerPC, SPARC)
+#  Potential improvement could be:
+#   - using 'network byte order' (big endian)
+#   - using BOM (add an identifier before all values)
+#  Another practical improvement if you don't know what data is coming
+#  could be checking the values to see if they are reasonable
+
+# Optimization
+# Signed 16-bit integer (int16)
+# Range: -32768 … 32767
+# -> Use int16 (2 bytes)
+# Another optimization would be to send raw bytes instead of ASCII
+# since ASCII inflates data size by 33% in this case (12 bytes for ASCII vs 8 raw bytes)
+
+# Why aren't we just sending the data in a human readable format?
+# Sending text characters would use more space!
+# Sensors/edge devices may be simple without such functionality or unwanted wasted resources
+# Binary values are not possible to interpret wrong (except endianness)
+
+# Sending arbitrary amount of data
+# - establish protocol to send length first then receive data
+#   -> first header with data length always same size, then data
+# - using a delimiter; parse chunks of data and split using agreed upon delimiter
 
 
 def client(conn: socket.socket, addr: tuple[str, int]) -> None:
     with conn:
-        data = conn.recv(1024)
+        data = conn.recv(BASE64_SIZE)
         if not data:
             conn.sendall(b"No data received")
             return
@@ -38,10 +66,10 @@ def client(conn: socket.socket, addr: tuple[str, int]) -> None:
             conn.sendall(msg)
             return
 
-        t = v1 * BASE**EXPONENT
-        h = v2 * BASE**EXPONENT
+        t = round(v1 * BASE**EXPONENT, 2)
+        h = round(v2 * BASE**EXPONENT, 2)
 
-        print(f"\n** Received from {addr[0]} **\nTemp: {t} C\nHumidity: {h} %")  # noqa: T201
+        print(f"\n** Received from {addr[0]} **\nTemperature: {t} C\nHumidity: {h} %")  # noqa: T201
         conn.sendall(b"Ok")
 
 
